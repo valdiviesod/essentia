391cedff6e0e1e5814c2edd479b3a4149be96512
Daniel Valdivieso <dzvaldiviesod@gmail.com>
2023-10-21 08:13:32 -0500

    js redirecciones

---
+++ public/js/mainProducts.js
@@ -0,0 +1,7 @@
+function redirectToWhatsApp() {
+    window.open('https://api.whatsapp.com/send?phone=TUNUMERO', '_blank');
+}
+
+function redirectToInstagram() {
+    window.open('https://www.instagram.com/TUUSUARIO', '_blank');
+}
