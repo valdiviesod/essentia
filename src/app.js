1ab2d1dc4e3fcc9459f247d65d3b802a280965ce
Daniel Valdivieso <dzvaldiviesod@gmail.com>
2023-10-21 19:15:38 -0500

    backend added

---
+++ src/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const userRouter = require('./routers/user');
+const itemRouter = require('./routers/item');
+const cartRouter = require('./routers/cart');
+const orderRouter = require('./routers/order');
+require('./db/mongoose');
+
+const port = process.env.PORT;
+
+const app = express();
+
+app.use(express.json());
+app.use(userRouter);
+app.use(itemRouter);
+app.use(cartRouter);
+app.use(orderRouter);
+
+const publicDirectory = path.join(__dirname, '../public');
+app.use(express.static(publicDirectory));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(publicDirectory, 'mainPage.html'));
+});
+
+app.listen(port, () => {
+    console.log('Server listening on port ' + port);
+});
