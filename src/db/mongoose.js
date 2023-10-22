f4a923938382d8cd413d81e65d9102509bab223b
Daniel Valdivieso <dzvaldiviesod@gmail.com>
2023-10-21 21:03:03 -0500

    Reboot backend

--- src/db/mongoose.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-}).then(() => {
-    console.log('Conexión a la base de datos exitosa');
-}).catch((error) => {
-    console.error('Error de conexión a la base de datos:', error);
-});
